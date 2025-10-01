4b24a750499850046f56adebf6aae9b386a6a77c
BrendaEBonillaG <82917836+BrendaEBonillaG@users.noreply.github.com>
2025-10-01 14:25:37 -0600

    dotenv
    ya esta configurado el env

--- src/modulos/clientes/rutas.js
+++ src/modulos/clientes/rutas.js
@@ -1,9 +1,11 @@
 const express = require('express');
+
+const respuesta = require('../../red/respuestas')
 const router = express.Router();
 
 // Definir las rutas
 router.get('/', function (req, res) {
-  res.send('Lista de clientes');
+  respuesta.success(req, res, 'Todo ok desde clientes', 200)
 });
 
 // Exportar el router para poder utilizarlo en otros archivos
