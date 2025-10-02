21fcc8c4666b327ec27510eff171a9be5a95500e
BrendaEBonillaG <82917836+BrendaEBonillaG@users.noreply.github.com>
2025-10-02 13:05:09 -0600

    DB conectada

--- src/DB/mysql.js
+++ src/DB/mysql.js
@@ -1,8 +1,42 @@
 const mysql = require('mysql');
 const config = require('../config');
 
+const dbconfig = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database
+}
+
+let conexion;
+
+function conMysql(){
+    conexion = mysql.createConnection(dbconfig);
+
+    conexion.connect((err) => {
+        if(err){
+            console.log('[db err]', err);
+            setTimeout(conMysql, 2000);
+        }else{
+            console.log('DB connectada');
+        }
+    })
+
+    conexion.on('error', err => {
+        console.log('[db err]', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            conMysql();
+        } else{
+            throw err;
+        }
+    });
+}
+
+
+conMysql();
+
 function todos(tabla){
-    return 'todos';
+    return prueba;
 
 }
 
